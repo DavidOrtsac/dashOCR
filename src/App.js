c52bfd099bbda168e03132e0f35955a9654c607e
DavidOrtsac <43810738+DavidOrtsac@users.noreply.github.com>
2023-04-17 02:05:37 +0800

    first commit I think

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
 import { useCallback, useEffect, useState } from 'react';
 import { createWorker } from 'tesseract.js';
 import './App.css';
+ 
+
+// Configurations and stuff
+
+const API_KEY = "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"
+// q: HOLD ON, THAT IS UNSAFE. How do I safely reference it from a .env file?
+// a: https://create-react-app.dev/docs/adding-custom-environment-variables/
+
+// Let's hope this works.
 
 function App() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [textResult, setTextResult] = useState("");
+  const [userInput, setUserInput] = useState('')
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
 
+/* 
+  const callOpenAIAPI = async () => {
+    
+    console.log("Calling the OpenAI API");
+
+    const APIBody = {
+      "model": "text-davinci-003",
+      "prompt": "Turn this into a story:" + {textResult},
+      "temperature": 0,
+      "max_tokens": 600,
+      "top_p": 1.0,
+      "frequency_penalty": 0.0,
+      "presence_penalty": 0.0
+    }
+
+    const response = await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + API_KEY
+      },
+      body: JSON.stringify({ textResult })
+    });
+
+    const data = await response.json();
+    const { output } = data;
+
+      console.log("man what", data);
+      setApiOutput(`${data}`);
+  }
+
+  console.log(apiOutput); */
+
+  
+
+  // For Inputs
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput({textResult});
+  };
+
+  // OCR Worker
   const worker = createWorker();
-
   const convertImageToText = useCallback(async () => {
     if(!selectedImage) return;
     await worker.load();
@@ -24,20 +77,43 @@ function App() {
   const handleChangeImage = e => {
     if(e.target.files[0]) {
       setSelectedImage(e.target.files[0]);
+      setUserInput({textResult});
     } else {
       setSelectedImage(null);
       setTextResult("")
+      setUserInput({textResult});
     }
   }
 
+  console.log({textResult});
+
   return (
     <div className="App">
-      <h1>ImText</h1>
-      <p>Gets words in image!</p>
+
+    {/* What the user sees */}
+
+      <h1>DashOCR</h1>
+      <p>Slightly more accurate OCR.</p>
       <div className="input-wrapper">
+
+    {/* Upload Files Here */}
+
         <label htmlFor="upload">Upload Image</label>
         <input type="file" id="upload" accept='image/*' onChange={handleChangeImage} />
       </div>
+
+      <br />
+
+    {/* Text Extraction Area */}
+    <div className="prompt-container">
+        <textarea type="hidden" className="prompt-box" placeholder="" value={textResult} onChange={onUserChangedText} />
+        <div className="prompt-buttons">
+      <div>
+        <button onClick={callOpenAIAPI}>Extract Text</button>
+      </div>
+      </div>
+      </div>
+    {/* Results */}
 
       <div className="result">
         {selectedImage && (
@@ -54,5 +130,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
